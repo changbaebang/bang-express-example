87985ae1d3b404ddfad73e8efb1e583f66d38b74
방창배 <bangchangbae@bangchangbaeui-MacBookPro.local>
2019-09-03 12:14:30 +0900

    pageshow is trigger for dable script

--- public/iframe-sample-reload/plugin.js
+++ public/iframe-sample-reload/plugin.js
@@ -1,6 +1,14 @@
-console.info("start plugin.js");
+//console.info("start plugin.js");
+//console.info(window["pageshow"]);
+//console.info(window["load"]);
+window.addEventListener("pageshow",() => console.info("pageshow"));
+window.addEventListener("load",() => console.info("load"));
 
-(function () { var define = undefined; (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+
+(function () {
+  console.info("script load");
+  var define = undefined; (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
   var CommandQueue, util;
 
   util = require('./util.coffee');
